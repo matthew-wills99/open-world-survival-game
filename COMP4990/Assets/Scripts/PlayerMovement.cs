fe6c2016113b0cd06cc74c9a64a013cb347d6265
Yusuf Naebkhil <yusuf.naeb@outlook.com>
2024-05-11 22:03:10 -0400

    Player+Animations
    Created Player and Animations. Player can move with WASD, Arrow keys, and Controller. Animations work, no diagonal walking yet.

---
+++ COMP4990/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+
+    public Animator animator;
+    Vector2 movement;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets input of the user (WASD, ARROW KEYS, CONTROLLER EVEN)
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        //Locks diagonal movement so it doesnt add both inputs
+        movement = Vector2.ClampMagnitude(movement, 1);
+
+        //Plays the correct animation with the speed+direction your moving (if speed >0.01 animation horizonatal or left is played)
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    private void FixedUpdate() {
+        //Moves the player
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
