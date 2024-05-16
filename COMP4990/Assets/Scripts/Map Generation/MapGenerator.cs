812d4bc1c311a602066a18b7ff6d3d085f6d7819
Matthew Wills <matthewwills146@gmail.com>
2024-05-15 20:02:32 -0400

    Restarted map generation
    Deleted a bunch of garbage, restarted map gen using perlin to generate biomes

--- COMP4990/Assets/Scripts/Map Generation/MapGenerator.cs
+++ COMP4990/Assets/Scripts/Map Generation/MapGenerator.cs
@@ -1,145 +1,68 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
-/*
-
-generate random terrain using perlin noise and a pre made noise map to ensure the spawn is always similar
-select random points on the map to be used for structures and landmarks
-generate the structures and landmarks using wave function collapse
-
-*/
-
-/*
-
-similar to core keeper, ground tile and wall tile only.
-
-*/
 
 public class MapGenerator : MonoBehaviour
 {
-    Dictionary<int, GameObject> tileset;
-    Dictionary<int, GameObject> tile_groups = null;
-    public GameObject pfb_plains;
-    public GameObject pfb_forest;
-    //public GameObject pfb_desert;
-    //public GameObject pfb_lake;
-    public GameObject pfb_ocean;
-    //public GameObject pfb_river;
-    public GameObject pfb_snow;
-    //public GameObject pfb_swamp;
+    public bool useColour = false;
 
-    int map_width = 160;
-    int map_height = 90;
+    public int width = 256;
+    public int height = 256;
 
-    List<List<int>> noise_grid = null;
-    List<List<GameObject>> tile_grid = null;
+    public float scale = 20f;
+    public float offsetX = 100f;
+    public float offsetY = 100f;
 
-    public float magnification = 7.0f;
-
-    // MAKE FLOAT
-    public int x_offset = 0;
-    public int y_offset = 0;
+    // TODO fix this shit
+    public Color[] biomes;
+    [Range(0f, 1f)]
+    public float[] biomeThresholds; // should be in ascending order and equal in length to biomes
+    
+    private new Renderer renderer;
 
     void Start()
     {
-        Generate();
+        renderer = GetComponent<Renderer>();
     }
 
-    [ContextMenu("Generate Map")]
-    public void Generate()
+    void Update()
     {
-        if(tile_grid != null)
+        renderer.material.mainTexture = GenerateTexture();
+    }
+
+    Texture2D GenerateTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+
+        for(int x = 0; x < width; x++)
         {
-            foreach(List<GameObject> row in tile_grid)
+            for(int y = 0; y < height; y++)
             {
-                foreach(GameObject obj in row)
+                Color colour = CalculateColour(x, y);
+                texture.SetPixel(x, y, colour);
+            }
+        }
+
+        texture.Apply();
+        return texture;
+    }
+
+    Color CalculateColour(int x, int y)
+    {
+        float xCoord = (float)x / width * scale + offsetX;
+        float yCoord = (float)y / height * scale + offsetY;
+
+        float sample = Mathf.Clamp(Mathf.PerlinNoise(xCoord, yCoord), 0f, 1f);
+
+        if(useColour)
+        {
+            // iterate thru biomes descending order
+            for(int i = biomes.Length - 1; i >= 0; i--)
+            {
+                if(sample >= biomeThresholds[i])
                 {
-                    Destroy(obj);
+                    return biomes[i]; // ? i think?
                 }
             }
         }
-
-        if(tile_groups != null)
-        {
-            foreach(KeyValuePair<int, GameObject> group in tile_groups)
-            {
-                Destroy(group.Value);
-            }
-        }
-
-        noise_grid = new List<List<int>>();
-        tile_grid = new List<List<GameObject>>();
-
-        CreateTileset();
-        GenerateTileGroups();
-        GenerateMap();
-    }
-
-    void CreateTileset()
-    {
-        tileset = new Dictionary<int, GameObject>
-        {
-            { 0, pfb_ocean },
-            { 1, pfb_plains },
-            { 2, pfb_forest },
-            { 3, pfb_snow }
-        };
-    }
-
-    public void GenerateTileGroups()
-    {
-        tile_groups = new Dictionary<int, GameObject>();
-        foreach(KeyValuePair<int, GameObject> prefab_pair in tileset)
-        {
-            GameObject tile_group = new GameObject(prefab_pair.Value.name);
-            tile_group.transform.parent = gameObject.transform;
-            tile_group.transform.localPosition = new Vector3(0, 0, 0);
-            tile_groups.Add(prefab_pair.Key, tile_group);
-        }
-    }
-
-    void GenerateMap()
-    {
-        for(int x = 0; x < map_width; x++)
-        {
-            noise_grid.Add(new List<int>());
-            tile_grid.Add(new List<GameObject>());
-            for(int y = 0; y < map_height; y++)
-            {
-                int tile_id = GetIdUsingPerlin(x, y);
-                // TODO: change this so we can adjust the ranges that tiles can spawn
-                noise_grid[x].Add(tile_id);
-                CreateTile(tile_id, x, y);
-            }
-        }
-    }
-
-    int GetIdUsingPerlin(float x, float y)
-    {
-        float raw_perlin = Mathf.PerlinNoise(
-            (x - x_offset) / magnification, 
-            (y - y_offset) / magnification
-        );
-
-        float clamped_perlin = Mathf.Clamp(raw_perlin, 0.0f, 1.0f);
-        float scaled_perlin = clamped_perlin * tileset.Count;
-        if(scaled_perlin == tileset.Count)
-        {
-            scaled_perlin = tileset.Count - 1;
-        }
-        return Mathf.FloorToInt(scaled_perlin);
-    }
-
-    void CreateTile(int tile_id, int x, int y)
-    {
-        GameObject tile_prefab = tileset[tile_id];
-        GameObject tile_group = tile_groups[tile_id];
-        GameObject tile = Instantiate(tile_prefab, tile_group.transform);
-
-        tile.name = string.Format("tile_x[0]_y[1]", x, y);
-        tile.transform.localPosition = new Vector3(x, y, 0);
-        
-        tile_grid[x].Add(tile);
+        return new Color(sample, sample, sample);
     }
 }
